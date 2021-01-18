5fbccf94a9953d78f1c848f0ab6e81c419ccfb09
Ivo-Vladimirov-Ganev <ivo.ganev@yahoo.com>
2021-01-18 09:38:32 +0000

    Create app document provider and add it to the Manifest.xml

--- app/src/main/java/com/ivo/ganev/awords/MainFragment.kt
+++ app/src/main/java/com/ivo/ganev/awords/MainFragment.kt
@@ -13,7 +13,5 @@ class MainFragment : Fragment(R.layout.main_fragment) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding = MainFragmentBinding.inflate(layoutInflater)
     }
-
 }
