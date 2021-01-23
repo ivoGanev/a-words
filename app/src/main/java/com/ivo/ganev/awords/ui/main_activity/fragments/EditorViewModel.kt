8dc2c99622d9c9476314d2e5b728e34fe13edecf
Ivo-Vladimirov-Ganev <ivo.ganev@yahoo.com>
2021-01-23 17:34:54 +0000

    Add saving functionality for file in editor view model

--- app/src/main/java/com/ivo/ganev/awords/ui/main_activity/fragments/EditorViewModel.kt
+++ app/src/main/java/com/ivo/ganev/awords/ui/main_activity/fragments/EditorViewModel.kt
@@ -1,5 +1,9 @@
 package com.ivo.ganev.awords.ui.main_activity.fragments
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -13,6 +17,9 @@ import com.ivo.ganev.datamuse_kotlin.endpoint.words.hardConstraintsOf
 import com.ivo.ganev.datamuse_kotlin.response.RemoteFailure
 import com.ivo.ganev.datamuse_kotlin.response.WordResponse
 import kotlinx.coroutines.launch
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.io.IOException
 
 class EditorViewModel : ViewModel() {
     private val datamuseClient = DatamuseKotlinClient()
@@ -56,6 +63,22 @@ class EditorViewModel : ViewModel() {
         viewModelScope.launch {
             val words = datamuseClient.query(query.build())
             words.applyEither({ _failure.postValue(it) }, { _results.postValue(it) })
+        }
+    }
+
+    fun save(context: Context,uri: Uri?, text: String) {
+        try {
+            if(uri!=null) {
+                val parcelFileDescriptor = context.contentResolver.openFileDescriptor(uri, "w")
+                val outputStream = FileOutputStream(parcelFileDescriptor?.fileDescriptor)
+                outputStream.write(text.encodeToByteArray())
+                outputStream.close()
+                parcelFileDescriptor?.close()
+            }
+        } catch (ex: FileNotFoundException) {
+            TODO()
+        } catch (ex: IOException) {
+            TODO()
         }
     }
 }
