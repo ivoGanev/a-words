52ed10c55127fdad811c7009d0dde7dd5bd60ed5
Ivo-Vladimirov-Ganev <ivo.ganev@yahoo.com>
2021-01-23 17:08:20 +0000

    Refactor editor activity and view model, and fix query bugs

--- app/src/main/java/com/ivo/ganev/awords/ui/main_activity/fragments/EditorFragment.kt
+++ app/src/main/java/com/ivo/ganev/awords/ui/main_activity/fragments/EditorFragment.kt
@@ -2,6 +2,8 @@ package com.ivo.ganev.awords.ui.main_activity.fragments
 
 import android.os.Bundle
 import android.view.View
+import android.widget.RadioButton
+import androidx.core.view.get
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
@@ -9,46 +11,26 @@ import com.ivo.ganev.awords.R
 import com.ivo.ganev.awords.databinding.FragmentEditorBinding
 import com.ivo.ganev.awords.ui.main_activity.MainActivity
 import com.ivo.ganev.awords.view.TextViewWordMutator
-import com.ivo.ganev.datamuse_kotlin.response.WordResponse
 import com.ivo.ganev.datamuse_kotlin.response.WordResponse.Element.Word
 
 class EditorFragment : Fragment(R.layout.fragment_editor) {
     private val viewModel: EditorViewModel by viewModels()
-    private lateinit var binding: FragmentEditorBinding
     private val args: EditorFragmentArgs by navArgs()
 
-    //the currently running instance of the activity
-    private val mainActivityContext: MainActivity by lazy {
-        activity as MainActivity
-    }
+    private lateinit var binding: FragmentEditorBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentEditorBinding.bind(view)
         binding.contentTextview.setClickableText(args.content)
 
-        when (binding.include.Type.checkedRadioButtonId) {
-            binding.include.button1.id -> viewModel.type = EditorViewModel.Type.Synonyms
-            binding.include.button2.id -> viewModel.type = EditorViewModel.Type.Antonyms
-            binding.include.button3.id -> viewModel.type = EditorViewModel.Type.Rhymes
-        }
 
-        binding.include.Type.setOnCheckedChangeListener { _, index ->
-            when (index) {
-                0 -> viewModel.type = EditorViewModel.Type.Synonyms
-                1 -> viewModel.type = EditorViewModel.Type.Antonyms
-                2 -> viewModel.type = EditorViewModel.Type.Rhymes
-            }
-        }
+        binding.include.button1.tag = EditorViewModel.RadioGroupType.Synonyms
+        binding.include.button2.tag = EditorViewModel.RadioGroupType.Antonyms
+        binding.include.button3.tag = EditorViewModel.RadioGroupType.Rhymes
 
-        binding.contentTextview.onWordClickedListener =
-            object : TextViewWordMutator.OnWordClickedListener {
-                override fun onWordClick(word: String) {
-                    val query = viewModel.makeQuery(word, viewModel.type)
-                    println(query.build().toUrl())
-                    viewModel.fireUpQuery(query)
-                }
-            }
+
+        binding.contentTextview.onWordClickedListener = onWordClickedListener()
 
         viewModel.results.observe(viewLifecycleOwner) { wordResponses ->
             val randomWord = wordResponses
@@ -57,6 +39,14 @@ class EditorFragment : Fragment(R.layout.fragment_editor) {
 
             if (randomWord.isNotEmpty())
                 binding.contentTextview.replaceSelectedWord(randomWord.random().word)
+        }
+    }
+
+    private fun onWordClickedListener() = object : TextViewWordMutator.OnWordClickedListener {
+        override fun onWordClick(word: String) {
+            val checkedId = binding.include.editorRadioGroup.checkedRadioButtonId
+            val type = requireActivity().findViewById<RadioButton>(checkedId).tag
+            viewModel.query(word, type as EditorViewModel.RadioGroupType)
         }
     }
 }
--- app/src/main/java/com/ivo/ganev/awords/ui/main_activity/fragments/EditorViewModel.kt
+++ app/src/main/java/com/ivo/ganev/awords/ui/main_activity/fragments/EditorViewModel.kt
@@ -1,10 +1,10 @@
 package com.ivo.ganev.awords.ui.main_activity.fragments
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ivo.ganev.datamuse_kotlin.client.DatamuseKotlinClient
-import com.ivo.ganev.datamuse_kotlin.endpoint.builders.WordsEndpointBuilder
 import com.ivo.ganev.datamuse_kotlin.endpoint.builders.wordsBuilder
 import com.ivo.ganev.datamuse_kotlin.endpoint.words.HardConstraint
 import com.ivo.ganev.datamuse_kotlin.endpoint.words.HardConstraint.*
@@ -17,30 +17,45 @@ import kotlinx.coroutines.launch
 class EditorViewModel : ViewModel() {
     private val datamuseClient = DatamuseKotlinClient()
 
-    lateinit var type: EditorViewModel.Type
+    private val _results = MutableLiveData<Set<WordResponse>>()
+    val results: LiveData<Set<WordResponse>>
+        get() = _results
 
-    enum class Type {
-        Synonyms,
-        Antonyms,
-        Rhymes
+    private val _failure = MutableLiveData<RemoteFailure>()
+    val failure: LiveData<RemoteFailure>
+        get() = _failure
+
+    enum class RadioGroupType {
+        Synonyms {
+            override fun toConstraint(word: String): List<HardConstraint> {
+                return hardConstraintsOf(RelatedWords(SYNONYMS, word))
+            }
+        },
+        Antonyms {
+            override fun toConstraint(word: String): List<HardConstraint> {
+                return hardConstraintsOf(RelatedWords(ANTONYMS, word))
+            }
+        },
+        Rhymes {
+            override fun toConstraint(word: String): List<HardConstraint> {
+                return hardConstraintsOf(RelatedWords(RHYMES, word))
+            }
+        };
+
+        abstract fun toConstraint(word: String): List<HardConstraint>
     }
 
-    fun makeQuery(word: String, type: Type) = wordsBuilder {
-        hardConstraints = when (type) {
-            Type.Synonyms -> hardConstraintsOf(RelatedWords(SYNONYMS, word))
-            Type.Antonyms -> hardConstraintsOf(RelatedWords(ANTONYMS, word))
-            Type.Rhymes -> hardConstraintsOf(RelatedWords(RHYMES, word))
+    /**
+     * Will query Datamuse and update [results] or [failure] accordingly.
+     * */
+    fun query(word: String, radioGroupType: RadioGroupType) {
+        val query = wordsBuilder {
+            hardConstraints = radioGroupType.toConstraint(word)
         }
-        maxResults = 10
-    }
-
-    private val failure = MutableLiveData<RemoteFailure>()
-    val results = MutableLiveData<Set<WordResponse>>()
-
-    fun fireUpQuery(query: WordsEndpointBuilder) {
+        println(query.build().toUrl())
         viewModelScope.launch {
             val words = datamuseClient.query(query.build())
-            words.applyEither({ failure.postValue(it) }, { results.postValue(it) })
+            words.applyEither({ _failure.postValue(it) }, { _results.postValue(it) })
         }
     }
 }
